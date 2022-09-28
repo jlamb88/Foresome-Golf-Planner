4f0dcbf86ad71da69506f731e889028581050aad
Myson Howard <mysonhoward@Mysons-Air.attlocal.net>
2022-09-28 13:49:03 -0400

    weather tracker

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 var weatherTracker = {
     "weatherapi": "ac96e744e22de6b8cf05d8399f7bfdf3",
     fetchtracker: function(){
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=ac96e744e22de6b8cf05d8399f7bfdf3")
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=+city+&appid=ac96e744e22de6b8cf05d8399f7bfdf3")
         .then((response) => response.json())
-        .then((data) => console.log(data));
-    }
+        .then((data) => this.forecastDisplay(data));
+    },
 
     forecastDisplay: function (data) {
         var { name } = data;
         var { speed } = data.wind;
-        var { description, icon } = data.weather;
+        var { description, icon } = data.weather[0];
         var { humidity, temp } = data.main;
         console.log(name, temp, description, icon, humidity, speed);
-    
-    },
+        
+    }
  
  
 }
