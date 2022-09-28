9cde6cc15547223e1bab8efd8b43f9e3cb216f41
Charlee Gordon <cgordon5025@gmail.com>
2022-09-28 13:31:44 -0400

    search bar complete

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-
-var searchBtn = $("#searchBtn")
+var searchBtn = $('#searchBtn');
+var cityInputEl = $('#cityInput');
+var radInputEl = $('#distanceInput');
+var searchContainerEl = $("#savedCities")
+var prevCities;
+var prevDistance;
+var savedSearched = []
 
 //Charlee note: rename all variables and formatting once the HTML is built
 searchBtn.click(saveSearch);
-cityContainter.on("click", "button", showPrevCity);
+searchContainerEl.on("click", "button", showPrevCity);
 function init() {
     //when the page loads we hide the weather display since they have yet to search for things
     //we also will call down items from local storage, and then we will render the cities
     weatherContainer.style.visibility = "hidden";
 
-    if (!localStorage.getItem("mySavedCities")) {
-        prevCities = [];
+    if (!localStorage.getItem("prevSearches")) {
+        savedSearched = [];
     } else {
-        prevCities = JSON.parse(localStorage.getItem("mySavedCities"));
+        savedSearched = JSON.parse(localStorage.getItem("prevSearches"));
     }
     renderCities();
 }
@@ -20,50 +25,52 @@ function init() {
 async function saveSearch(event) {
     //when we save we don't want something to be removed from the search bar
     event.preventDefault();
-    //now we want to save the data and also send the data to an array and display it
-    var cityText = inputEl.value.trim();
-    if (cityText !== "") {
-        prevCities.push(cityText);
+    var currentSearch = {
+        cityText: cityInputEl.val(),
+        radiusText: radInputEl.val()
     }
-    city = cityText;
+    console.log(currentSearch)
+    if ((currentSearch.cityText !== '') && (currentSearch.radiusText !== '')) {
+        savedSearched.push(currentSearch)
+    } else (savedSearched = [])
+    //now we want to save the data and also send the data to an array and display it
+    zipcode = currentSearch.cityText;
     //We want to clear these variables so that they are empty if we call a new city
-    nextForecast = [];
-    futureArray = [];
+    // nextForecast = [];
+    // futureArray = [];
     storeCities();
     renderCities();
-    // getAPI().then(function (res) {
-    // });
-    // runFuture();
-
+    //add lines functions that call API and display the information
 }
 
 function storeCities() {
     //lets store them locally so when we reload the page they'll still be here
-    localStorage.setItem("mySavedCities", JSON.stringify(prevCities));
+    localStorage.setItem("prevSearches", JSON.stringify(savedSearched));
 }
 function renderCities() {
-    cityList.innerHTML = "";
+    searchContainerEl.innerHTML = "";
 
-    for (var i = 0; i < prevCities.length; i++) {
+    for (var i = 0; i < savedSearched.length; i++) {
         //lets add the all of the previous cities as buttons
-        var searchText = prevCities[i];
+        var searchText = "zipcode: " + savedSearched[i].cityText + ' : ' + savedSearched[i].radiusText + " mile radius";
+        var prevBtn = $("<button></button>");
 
-        var cityBtn = document.createElement("button");
-        var btnContainer = document.createElement("li");
+        var btnContainer = $("<li></li>");
 
-        cityBtn.text(searchText);
-        cityBtn.classList.add('w-100')
-        btnContainer.append(cityBtn);
-        cityList.append(btnContainer);
+        prevBtn.text(searchText);
+        prevBtn.addClass('w-100')
+        btnContainer.append(prevBtn);
+        searchContainerEl.append(btnContainer);
     }
 }
 function extractCity(event) {
-    //if they pick a previously searched city we want to extract whatever information is in the button they selected
     var cityInput = event.currentTarget.innerHTML;
-    city = cityInput;
-    return city;
+    zipcode = cityInput;
+    return zipcode;
 }
 var todayWeather;
+
+//replace this code with the same to execute the API and display elements
 function showPrevCity(event) {
     //if they want to see a previous city we will first extract the info and then run the same info that would happen if they just search for it
     extractCity(event);
