d8f20017603f7a9f27cd6f7f36716392fa5e350d
Myson Howard <mysonhoward@Mysons-Air.attlocal.net>
2022-09-29 09:44:30 -0400

    weather tracker

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var weatherTracker = {
     "weatherapi": "ac96e744e22de6b8cf05d8399f7bfdf3",
     fetchtracker: function(){
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=+city+&appid=ac96e744e22de6b8cf05d8399f7bfdf3")
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=+city+&metric=imerial&appid=ac96e744e22de6b8cf05d8399f7bfdf3")
         .then((response) => response.json())
         .then((data) => this.forecastDisplay(data));
     },
 
-    forecastDisplay: function (data) {
+    display: function (data) {
         var { name } = data;
         var { speed } = data.wind;
         var { description, icon } = data.weather[0];
