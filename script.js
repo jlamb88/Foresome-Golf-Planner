b61c438e937ed2b69ae21f797dde9bcf2a299b62
Joseph Lamb <jlambpco@gmail.com>
2022-10-02 16:03:34 -0400

    completed golf API with formatted search cards and updated HTML layout for page

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-
-// for (i=0;i<tempArry.length;i++) {
-//              coursesArry.push(tempArry[i])
-//        console.log("fetch",coursesArry);
-//     }
-
-//     return coursesArry;
-//    var distArray = []; //Array of Objects that only contain the name and the zipcode from the first call
-//    for(var i = 0; i < tempArry.length; i++) {
-//        distArray.push({name: tempArry[i].name, zip_code: tempArry[i].zip_code});
-
-// *********************************API CODE************************************
+// Nested API to pull golf course search info based on location and radius input
 var srchRadius = 5;
 var userLat = 33.749;
 var userLong = -84.38798;
@@ -24,17 +13,17 @@ const listOptions = {
     }
 };
 
+// first API fetch to retrieve general course results in a geographic area from search
 fetch(passURL, listOptions)
 	.then(response => response.json())
 	.then(async function(response)
             {
             var coursesList = response.courses;
-            courseResultsCount = coursesList.length + ' courses meet your search criteria';
+            courseResultsCount = '<br>'+coursesList.length + ' courses meet your search criteria<br>';
             $('#courses-bar').append('<h6>'+courseResultsCount+'</h6></br>');
-            var endResults = []; 
+//iterates second fetch call for each course fetched by first results
             for(var i = 0; i < coursesList.length; i++) 
                 {
-                var courseObj = {name: coursesList[i].name, distance: coursesList[i].distance, address: "",image: "",URL: ""}
                 var courseDistance = coursesList[i].distance;
                 var courseName = coursesList[i].name;
                 await secondUrlFetchCall(coursesList[i]);
@@ -60,19 +49,15 @@ fetch(passURL, listOptions)
                     {
                     var courseAddress = response.course_details.result.formatted_address
                     var courseURL = response.course_details.result.website
-                    var placesImgKey = 'AIzaSyDwPSRdwH9WbRXsdvun20zY-AuIQhzuqeU'
+                    var placesImgKey = 'AIzaSyDwPSRdwH9WbRXsdvun20zY-AuIQhzuqeU' //Google Places API key
                     var courseImgRef = response.course_details.result.photos[0].photo_reference
-                    var courseImg = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&maxheight=75&photo_reference='+courseImgRef+'&key='+placesImgKey
+                    var courseImg = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=2000&photo_reference='+courseImgRef+'&key='+placesImgKey
                     var courseName = input.name
                     var courseDistance = input.distance
-                    // courseImg = "image"
-                    // courseName = "Bobby Jones"
-                    // courseAddress = "123 Main St"
-                    // courseDistance = "3.6 mi."
-                    // courseURL = "test"
                     var courseResultsCard =
-                    '<div class="card" id="course-card" style="width: 11rem;height: 25rem;margin: 0 1rem 1rem 0"> <img src="'+courseImg+'" class="card-img-top" id="card-image" alt="picture of golf course from search results"> <div class="card-body"> <h6 class="card-title">'+courseName+'</h6> <p class="card-text" id="card-address">'+courseAddress+'<br>'+courseDistance+' mi.</br></p> <a href="'+courseURL+'" class="btn btn-primary" id="card-URL">Website</a> </div> </div>';
-                
+                    '<div class="card" id="course-card" style="width: 12.1rem;height: 28rem;margin: 0 0.75rem 0.75rem 0"> <img src="'+courseImg+'" class="card-img-top" id="card-image" alt="picture of golf course from search results"> <div class="card-body"> <h6 class="card-title">'+courseName+'</h6></br> <p class="card-text" id="card-address">'+courseAddress+'<br>'+courseDistance+' mi.</br></p> <a href="'+courseURL+'" class="btn btn-primary" id="card-URL">Website</a> </div> </div>';
+                    
+// Write search results card to page
                 $('#courses-results').append(courseResultsCard);
                     return;
                     }
@@ -81,3 +66,5 @@ fetch(passURL, listOptions)
                 };
             }
     )
+
+    
